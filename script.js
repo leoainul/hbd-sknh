31f88edeef7b88d9270c01414ed57fce0dd5f648
Moh. Ainul Yaqin <113249567+leoainul@users.noreply.github.com>
2024-07-07 23:31:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var audio = document.getElementById("laguku");
+
+function ultah() {
+  var pesanhbd = document.getElementById("pesanhbd");
+  pesanhbd.classList.remove("hidden");
+  audio.play();
+}
